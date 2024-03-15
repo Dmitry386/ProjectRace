9aba9fe92c4405a24d4e5ae6d54ac94041e4cd66
Dmitry Volkov <dimon.volk00@gmail.com>
2024-03-16 00:17:59 +0200

    Добавьте файлы проекта.

---
+++ Assets/Scripts/Core/Bootstrap.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Assets.Scripts.Core
+{
+    internal class Bootstrap : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+
+        }
+
+        private void ContainerBindInstance<T>() where T : Object
+        {
+            Container.BindInstance<T>(GameObject.FindAnyObjectByType<T>());
+        }
+    }
+}
