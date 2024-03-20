b47c59099240df97488391cf45032e4f2a7bfe27
Dmitry Volkov <dimon.volk00@gmail.com>
2024-03-20 12:24:08 +0200

    level timer

---
+++ Assets/Scripts/UI/Garage/TextDisplays/LocationTimerUI.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts.World.Locations;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts.UI.Garage.TextDisplays
+{
+    internal class LocationTimerUI : MonoBehaviour
+    {
+        [SerializeField] private string _printFormat = "TIME REMAINING: {0}";
+        [SerializeField] private LocationTimer _timer;
+        [SerializeField] private TextMeshProUGUI _textMeshPro;
+
+        private void Update()
+        {
+            if (_timer)
+            {
+                UpdateValue(_timer.GetRemainingTime());
+            }
+            else
+            {
+                UpdateValue(0);
+            }
+        }
+
+        private void UpdateValue(float v)
+        {
+            _textMeshPro.text = string.Format(_printFormat,((int)v).ToString());
+        }
+    }
+}
---
+++ Assets/Scripts/World/Locations/LocationTimer.cs
@@ -0,0 +1,58 @@
+﻿using Assets.Scripts.Core.Networking;
+using Assets.Scripts.Core.Networking.Definitions;
+using DVUnityUtilities.Other.Coroutines;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Scripts.World.Locations
+{
+    internal class LocationTimer : MonoBehaviour
+    {
+        [SerializeField] private float _timeInSeconds = 120f;
+
+        [Inject] private LocationSystem _locationSystem;
+        [Inject] private INetworkControl _networkControl;
+
+        private CoroutineTimer _timer;
+        private float _startTimerMoment;
+
+        private void Awake()
+        {
+            _locationSystem.OnLocationChanged += OnLocationChanged;
+        }
+
+        private void OnTimeIsOver()
+        {
+            _locationSystem.SetLocation(locationPrefab: null);
+        }
+
+        private void OnLocationChanged(Location obj)
+        {
+            StartTimer();
+        }
+
+        private void StartTimer()
+        {
+            if (_networkControl.GetNetworkStatus() == NetworkStatus.Host)
+            {
+                _timer?.Dispose();
+                _timer = new CoroutineTimer(this, _timeInSeconds, false).Start();
+                _timer.OnTick += OnTick;
+                _startTimerMoment = Time.time;
+            }
+        }
+
+        private void OnTick(CoroutineTimer obj)
+        {
+            if (_networkControl.GetNetworkStatus() == NetworkStatus.Host)
+            {
+                OnTimeIsOver();
+            }
+        }
+
+        public float GetRemainingTime()
+        {
+            return _timeInSeconds - Mathf.Clamp(Time.time - _startTimerMoment, 0, _timeInSeconds);
+        }
+    }
+}
